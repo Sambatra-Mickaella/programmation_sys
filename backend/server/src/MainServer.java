52bf31126ca63b292a5e46fd3f36afdbeb527e2b
Andriantsoa94 <voaharyralambo@gmail.com>
2026-02-03 15:20:07 +0300

    Refactor MainServer to use dynamic port from arguments

--- backend/server/src/MainServer.java
+++ backend/server/src/MainServer.java
@@ -3,10 +3,14 @@ import java.net.Socket;
 
 public class MainServer {
     public static void main(String[] args) {
+        int port = 2121;
+        if (args.length > 0) {
+            try { port = Integer.parseInt(args[0]); } catch(Exception ignored) {}
+        }
         try{
-            ServerSocket serverSocket = new ServerSocket(2121);
+            ServerSocket serverSocket = new ServerSocket(port);
             FileManager fm = new FileManager();
-            System.out.println("Server running on port 2121...");
+            System.out.println("Server running on port " + port + "...");
 
             while(true){
                 Socket client = serverSocket.accept();
